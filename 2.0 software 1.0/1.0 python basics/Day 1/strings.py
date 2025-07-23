a35f5fb8d3665ac2a9ebe63b57d301bd4e1a9132
Gadfomo <fomogad@gmail.com>
2025-07-23 15:37:29 +0100

    implimenting the spit method in an email and manipulating the string to uppercase, title case, length and cheching if a word is availbable in the string

--- 2.0 software 1.0/1.0 python basics/Day 1/strings.py
+++ 2.0 software 1.0/1.0 python basics/Day 1/strings.py
@@ -16,16 +16,34 @@
 # message = "Hello, python!"
 # print(len(message))
 
-name = "Alice"
-age = 30
-score = 95.5
+# name = "Alice"
+# age = 30
+# score = 95.5
 
-#method 1: f-strings recommend like fill in the blank
-print(f"Hello, {name}! You are {age} years old.")
-print(f"Your score is {score:.1f}%")
+# #method 1: f-strings recommend like fill in the blank
+# print(f"Hello, {name}! You are {age} years old.")
+# print(f"Your score is {score:.1f}%")
 
-#method 2: format() method
-print("Hello, {}! You are {} years old." .format(name,age))
+# #method 2: format() method
+# print("Hello, {}! You are {} years old." .format(name,age))
 
-#method 3: % formatting (older style)
-print("Hello, %s! You are %d years old." %(name, age))
+# #method 3: % formatting (older style)
+# print("Hello, %s! You are %d years old." %(name, age))
+
+email = input("Enter your email ")
+if "@" in email and "." in email:
+    username = email.split("@")[0]
+    domain = email.split("@")[1]
+    print(f"Username: {username}")
+    print(f"Domain: {domain}")
+else:
+    print("Invalid email format")
+
+
+text = "The quick brown fox jumps over the lazy dog"
+print(f"Text: {text}")
+print(f"Lenght: {len(text)} characters")
+print(f"Words: {len(text.split())} words")
+print(f"Uppercase: {text.upper()}")
+print(f"Title case: {text.title()}")
+print(f"Contains 'fox': {'fox' in text}")
