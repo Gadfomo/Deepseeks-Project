c37e474497ef8f74fcd7d86a7650c5be51503746
Gadfomo <fomogad@gmail.com>
2025-07-23 14:55:13 +0100

    exploring concatination, interpolation and getting the length of a string

---
+++ 2.0 software 1.0/1.0 python basics/Day 1/strings.py
@@ -0,0 +1,17 @@
+#concatination and interpolation
+
+
+#concatination
+first_name = "John"
+last_name = "Ven"
+full_name = first_name + last_name
+
+#interpolation
+print(f"my fullname is {full_name}")
+
+laugh = "Ha" * 7
+print(laugh)
+
+#length
+message = "Hello, python!"
+print(len(message))
