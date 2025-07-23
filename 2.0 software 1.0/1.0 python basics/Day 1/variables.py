b6ab6273746de1802116d14b8036642a0aa5e592
Gadfomo <fomogad@gmail.com>
2025-07-23 14:07:55 +0100

    I am done with variables

---
+++ 2.0 software 1.0/1.0 python basics/Day 1/variables.py
@@ -0,0 +1,34 @@
+# whole_number = 42
+# decimal_number = 3.14432
+# complex_number = 2 + 3j
+
+# greeting = "Hello Infidels"
+# single_char = 'A'
+
+# is_sunny = True
+# is_raining = False
+
+# print(type(whole_number))
+# print(type(greeting))
+# print(type(complex_number))
+
+# first_name= "Jeff"
+# last_name = "Dunan"
+
+# combine_name = first_name + " " + last_name
+# print("my full name is " + combine_name )
+# print(f"my full name is {combine_name}")
+
+
+#ban name generator
+
+#collecting data
+name=input("What is your name? ")
+age=input("How old are you? ")
+school=input("Which school do you attend? ")
+department=input("What department are you in? ")
+fav_meal=input("What is your favorite meal? ")
+best_friend=input("Who is your best friend? ")
+
+#creating a story
+print(f"My name is {name} and I am {age} years old. I am a student in {school} in the {department}. My love eating {fav_meal} and my best friend is {best_friend}.")
